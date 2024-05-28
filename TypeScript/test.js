44c4821e80cd6b67cd73c4616b62bae63ca0f615
CodeFit <codefit@qq.com>
2024-05-28 18:10:30 +0800

    01_TS 概述

---
+++ TypeScript/test.js
@@ -0,0 +1,9 @@
+// var hello = (name) => {
+//     return `Hello, CodeFit`
+// }
+// hello('CodeFit')
+var hello = function (name) {
+    return "Hello, {name}";
+};
+// hello('CodeFit!')
+hello(123);
---
+++ TypeScript/test.ts
@@ -0,0 +1,12 @@
+// var hello = (name) => {
+//     return `Hello, CodeFit`
+// }
+
+// hello('CodeFit')
+
+const hello = (name: string) => {
+    return `Hello, {name}`
+}
+
+// hello('CodeFit!')
+hello(123)
